1818b8aaa2aef08a12e37790683b0acc24ab3eba
Vincent Weevers <mail@vincentweevers.nl>
2015-11-17 10:51:04 +0100

    Take manifest version from package.json

--- src/build/templates/tasks/compress.js
+++ src/build/templates/tasks/compress.js
@@ -2,9 +2,8 @@ var gulp = require('gulp')
   , zip = require('gulp-zip')
 
 gulp.task('compress', () => {
-  let { version } = require('../app/manifest.json')
-    , { name } = require('../package.json')
-  
+  let { name, version } = require('../package.json')
+
   return gulp.src('dist/**/*')
     .pipe(zip(`${name}-${version}.zip`))
     .pipe(gulp.dest('package'))
